53b295ff7767249b20084276e3bda68f8eb976f0
Ali Barış Gültekin <alibarisgultekin@gmail.com>
2025-01-14 14:24:26 +0300

    Add TaskInfo component into boaed

--- src/app/components/Board/Board.tsx
+++ src/app/components/Board/Board.tsx
@@ -1,37 +1,82 @@
 "use client";
-import React from "react";
-import BoardColumn from "../BoardColumn/BoardColumn";
+import React, { useState } from "react";
 import styles from "./Board.module.scss";
+
+import BoardColumn from "../BoardColumn/BoardColumn";
+import Modal from "../Modal/Modal";
+import TaskInfo from "../TaskInfo/TaskInfo";
 
 import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "../../redux/store";
 import { updateTaskStatus } from "@/app/redux/slices/tasksSlice";
-
-import Modal from "../Modal/Modal";
 
 const Board = () => {
   // Access tasks from Redux store
   const tasks = useSelector((state: RootState) => state.tasks);
   const dispatch = useDispatch();
 
+  // Modal states
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  interface Task {
+    id: number;
+    title: string;
+    description: string;
+    status: string; // "Open", "In Progress", "In Review", "Done"
+    assigneeId: number;
+    storyPoints: number;
+    startDate: string;
+    endDate: string;
+  }
+
+  const [selectedTask, setSelectedTask] = useState<Task | null>(null);
+
   // Handle dropping a task into a new column
   const handleDrop = (taskId: number, newStatus: string) => {
     dispatch(updateTaskStatus({ id: taskId, status: newStatus }));
   };
 
+  // Modal handlers
+  const openModal = (taskId: number) => {
+    const task = tasks.find((t) => t.id === taskId);
+    if (task) {
+      setSelectedTask(task);
+    }
+    setIsModalOpen(true);
+  };
+
+  // Close modal
   const closeModal = () => {
-    console.log("foo")
+    setIsModalOpen(false);
+    setSelectedTask(null);
   };
 
   return (
     <div className={styles.board}>
-      <Modal onClose={closeModal}/>
+      {isModalOpen && (
+        <Modal onClose={closeModal}>
+          {selectedTask && (
+            <TaskInfo
+              id={selectedTask.id}
+              title={selectedTask.title}
+              description={selectedTask.description}
+              status={selectedTask.status}
+              assigneeId={selectedTask.assigneeId}
+              storyPoints={selectedTask.storyPoints}
+              startDate={selectedTask.startDate}
+              endDate={selectedTask.endDate}
+            />
+          )}
+        </Modal>
+      )}
       {["Open", "In Progress", "In Review", "Done"].map((status) => (
         <BoardColumn
           key={status}
           title={status}
-          tasks={tasks.filter((task: { status: string; }) => task.status === status)}
+          tasks={tasks.filter(
+            (task: { status: string }) => task.status === status
+          )}
           onDrop={handleDrop}
+          onTaskClick={openModal}
         />
       ))}
     </div>
