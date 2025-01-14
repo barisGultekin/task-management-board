ae70ca2e4aaea06a2f2b5c62757053032a67e18a
Ali Barış Gültekin <alibarisgultekin@gmail.com>
2025-01-14 14:23:50 +0300

    Fix type declaration issue

--- src/app/components/TaskCard/TaskCard.tsx
+++ src/app/components/TaskCard/TaskCard.tsx
@@ -9,9 +9,10 @@ interface TaskCardProps {
   id: number;
   title: string;
   assigneeId: number;
+  onClick: () => void;
 }
 
-const TaskCard: React.FC<TaskCardProps> = ({ id, title, assigneeId }) => {
+const TaskCard: React.FC<TaskCardProps> = ({ id, title, assigneeId, onClick }) => {
   // Access users from the Redux store
   const users = useSelector((state: RootState) => state.users);
   interface User {
@@ -31,12 +32,13 @@ const TaskCard: React.FC<TaskCardProps> = ({ id, title, assigneeId }) => {
       className={styles.card}
       draggable
       onDragStart={handleDragStart}
+      onClick={onClick}
     >
       <div className={styles.taskInfo}>
         <p>{title}</p>
         {assignee && (
           <div className={styles.assignee}>
-            <Image src={assignee.avatar} alt={assignee.name} className={styles.avatar} width={36} height={36}/>
+            <Image src={assignee.avatar} alt={assignee.name} className={styles.avatar} width={28} height={28}/>
             <p>{assignee.name}</p>
           </div>
         )}
