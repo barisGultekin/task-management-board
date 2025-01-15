b03ba94e88227c360960ba2a6a361c7776a4d0f8
Ali Barış Gültekin <alibarisgultekin@gmail.com>
2025-01-15 14:26:59 +0300

    Pass edit and delete functions to taskInfo and implement buttons

--- src/app/components/TaskInfo/TaskInfo.tsx
+++ src/app/components/TaskInfo/TaskInfo.tsx
@@ -1,9 +1,13 @@
 "use client";
 import React from "react";
 import styles from "./TaskInfo.module.scss";
+
 import { useSelector } from "react-redux";
 import { RootState } from "../../redux/store";
+
 import Image from "next/image";
+
+import { FaPenToSquare, FaTrashCan } from "react-icons/fa6";
 
 interface TaskInfoProps {
   id: number;
@@ -14,6 +18,8 @@ interface TaskInfoProps {
   storyPoints: number;
   startDate: string;
   endDate: string;
+  onEdit: (taskId: number) => void;
+  onDelete: (taskId: number) => void;
 }
 
 const TaskInfo: React.FC<TaskInfoProps> = ({
@@ -25,33 +31,79 @@ const TaskInfo: React.FC<TaskInfoProps> = ({
   storyPoints,
   startDate,
   endDate,
+  onEdit,
+  onDelete,
 }) => {
   // Access users from Redux to get assignee details
   const users = useSelector((state: RootState) => state.users);
   const assignee = users.find((user) => user.id === assigneeId);
 
-  console.log("Task ID:", id)
+  console.log("Task ID:", id);
 
   return (
     <div className={styles.taskInfo}>
-      <h2>{title}</h2>
-      <p>{description}</p>
-      <p>Status: {status}</p>
-      <p>Story Points: {storyPoints}</p>
-      <p>Start Date: {startDate}</p>
-      <p>End Date: {endDate}</p>
-      {assignee && (
-        <div className={styles.assignee}>
-          <Image
-            src={assignee.avatar}
-            alt={assignee.name}
-            className={styles.avatar}
-            width={50}
-            height={50}
-          />
-          <span>{assignee.name}</span>
+      <div className={styles.header}>
+        <h2>{title}</h2>
+        <p>{description}</p>
+      </div>
+
+      <div className={styles.infoTable}>
+        <div className={styles.infoRow}>
+          <div className={styles.tag}>
+            <p>Status</p>
+          </div>
+          <p>{status}</p>
         </div>
-      )}
+        <div className={styles.infoRow}>
+          <div className={styles.tag}>
+            <p>Assignee</p>
+          </div>
+          {assignee && (
+            <div className={styles.assignee}>
+              <Image
+                src={assignee.avatar}
+                alt={assignee.name}
+                className={styles.avatar}
+                width={24}
+                height={24}
+              />
+              <p>{assignee.name}</p>
+            </div>
+          )}
+        </div>
+
+        <div className={styles.infoRow}>
+          <div className={styles.tag}>
+            <p>Story Points</p>
+          </div>
+          <p> {storyPoints}</p>
+        </div>
+
+        <div className={styles.infoRow}>
+          <div className={styles.tag}>
+            <p>Start</p>
+          </div>
+          <p> {startDate}</p>
+        </div>
+
+        <div className={styles.infoRow}>
+          <div className={styles.tag}>
+            <p>End</p>
+          </div>
+          <p> {endDate}</p>
+        </div>
+      </div>
+
+      <div className={styles.actions}>
+        <button type="button" onClick={() => {onEdit(id)}}>
+          <FaPenToSquare />
+          <p>Edit</p>
+        </button>
+        <button type="button" onClick={() => {onDelete(id)}}>
+          <FaTrashCan />
+          <p>Delete</p>
+        </button>
+      </div>
     </div>
   );
 };
