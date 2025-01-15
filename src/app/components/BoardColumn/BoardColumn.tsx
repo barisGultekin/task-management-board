7aa6249d1721d2a908c548fad08e24758eb92479
Ali Barış Gültekin <alibarisgultekin@gmail.com>
2025-01-15 14:25:49 +0300

    Pass edit and delete methods to board column

--- src/app/components/BoardColumn/BoardColumn.tsx
+++ src/app/components/BoardColumn/BoardColumn.tsx
@@ -4,12 +4,23 @@ import styles from "./BoardColumn.module.scss";
 
 interface BoardColumnProps {
   title: string;
+  taskCount: number;
   tasks: { id: number; title: string; status: string; assigneeId: number }[];
   onDrop: (taskId: number, newStatus: string) => void;
   onTaskClick: (taskId: number) => void;
+  onTaskEdit: (taskId: number) => void;
+  onTaskDelete: (taskId: number) => void;
 }
 
-const BoardColumn: React.FC<BoardColumnProps> = ({ title, tasks, onDrop, onTaskClick }) => {
+const BoardColumn: React.FC<BoardColumnProps> = ({
+  title,
+  taskCount,
+  tasks,
+  onDrop,
+  onTaskClick,
+  onTaskEdit,
+  onTaskDelete,
+}) => {
   const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
     e.preventDefault();
   };
@@ -19,22 +30,39 @@ const BoardColumn: React.FC<BoardColumnProps> = ({ title, tasks, onDrop, onTaskC
     onDrop(taskId, title);
   };
 
+  const titleColor =
+    title === "Open"
+      ? styles.open
+      : title === "In Progress"
+        ? styles.inProgress
+        : title === "In Review"
+          ? styles.inReview
+          : styles.done;
+
   return (
     <div
       className={styles.column}
       onDragOver={handleDragOver}
       onDrop={handleDrop}
     >
-      <h2>{title}</h2>
-      {tasks.map((task) => (
-        <TaskCard
-          key={task.id}
-          id={task.id}
-          title={task.title}
-          assigneeId={task.assigneeId} // Pass assigneeId to TaskCard
-          onClick={() => onTaskClick(task.id)}
-        />
-      ))}
+      <div className={styles.header}>
+        <div className={`${styles.title} ${titleColor}`}>{title}</div>
+        <p>{taskCount}</p>
+      </div>
+
+      <div className={styles.list}>
+        {tasks.map((task) => (
+          <TaskCard
+            key={task.id}
+            id={task.id}
+            title={task.title}
+            assigneeId={task.assigneeId} // Pass assigneeId to TaskCard
+            onClick={() => onTaskClick(task.id)}
+            onEdit={() => onTaskEdit(task.id)}
+            onDelete={() => onTaskDelete(task.id)}
+          />
+        ))}
+      </div>
     </div>
   );
 };
