5a3fad0bd32b2a36506301fc2ec1470e74fe8821
Ali Barış Gültekin <alibarisgultekin@gmail.com>
2025-01-15 14:26:30 +0300

    Add edit and delete buttons to the task card

--- src/app/components/TaskCard/TaskCard.tsx
+++ src/app/components/TaskCard/TaskCard.tsx
@@ -2,17 +2,28 @@
 import React from "react";
 import styles from "./TaskCard.module.scss";
 import { useSelector } from "react-redux";
-import { RootState } from "../../redux/store"
+import { RootState } from "../../redux/store";
 import Image from "next/image";
+
+import { FaPenToSquare, FaTrashCan } from "react-icons/fa6";
 
 interface TaskCardProps {
   id: number;
   title: string;
   assigneeId: number;
   onClick: () => void;
+  onEdit: () => void;
+  onDelete: () => void;
 }
 
-const TaskCard: React.FC<TaskCardProps> = ({ id, title, assigneeId, onClick }) => {
+const TaskCard: React.FC<TaskCardProps> = ({
+  id,
+  title,
+  assigneeId,
+  onClick,
+  onEdit,
+  onDelete,
+}) => {
   // Access users from the Redux store
   const users = useSelector((state: RootState) => state.users);
   interface User {
@@ -21,7 +32,9 @@ const TaskCard: React.FC<TaskCardProps> = ({ id, title, assigneeId, onClick }) =
     avatar: string;
   }
 
-  const assignee: User | undefined = users.find((user: User) => user.id === assigneeId);
+  const assignee: User | undefined = users.find(
+    (user: User) => user.id === assigneeId
+  );
 
   const handleDragStart = (e: React.DragEvent<HTMLDivElement>) => {
     e.dataTransfer.setData("text/plain", id.toString());
@@ -34,11 +47,37 @@ const TaskCard: React.FC<TaskCardProps> = ({ id, title, assigneeId, onClick }) =
       onDragStart={handleDragStart}
       onClick={onClick}
     >
+      <div className={styles.actions}>
+        <div
+          className={`${styles.action} ${styles.edit}`}
+          onClick={(e) => {
+            e.stopPropagation();
+            onEdit();
+          }}
+        >
+          <FaPenToSquare />
+        </div>
+        <div
+          className={`${styles.action} ${styles.delete}`}
+          onClick={(e) => {
+            e.stopPropagation();
+            onDelete();
+          }}
+        >
+          <FaTrashCan />
+        </div>
+      </div>
       <div className={styles.taskInfo}>
         <p>{title}</p>
         {assignee && (
           <div className={styles.assignee}>
-            <Image src={assignee.avatar} alt={assignee.name} className={styles.avatar} width={28} height={28}/>
+            <Image
+              src={assignee.avatar}
+              alt={assignee.name}
+              className={styles.avatar}
+              width={24}
+              height={24}
+            />
             <p>{assignee.name}</p>
           </div>
         )}
